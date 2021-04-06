379a7b8dd0544a6c5907db1b600b35c19f2c38ba
Zeus <hidarizeus@gmail.com>
2021-04-06 14:37:14 +0800

    fix git merge error

--- lib/items.dart
+++ lib/items.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 class DrawerItem {
   final IconData? icon;
   final Text title;
-<<<<<<< HEAD
   final Widget? content;
   final List<DrawerItem>? sub_content;
   DrawerItem({this.icon, required this.title, this.content, this.sub_content});
-=======
-  final Widget content;
-  DrawerItem({this.icon, required this.title, required this.content});
->>>>>>> b642b10 (search fix)
 }
