1ea925f4d81b2a7763d3d1dab8bc46b6c6355cdd
Zeus <hidarizeus@gmail.com>
2021-04-06 13:54:49 +0800

    revamped

--- lib/drawer_children/widget.dart
+++ lib/drawer_children/widget.dart
@@ -134,3 +134,115 @@ class _ZDrawerWidgetState extends State<ZDrawerWidget> {
     );
   }
 }
+
+
+//class ZDrawerWidget extends StatelessWidget {
+//  List<DrawerItem> items;
+//  Widget? title;
+//  Color bgColor;
+//  final BuildContext context;
+//  final bool isMob;
+//  ZDrawerWidget({required this.items, this.isMob = false, this.title,required this.bgColor,required this.context});
+//  @override
+//  Widget build(BuildContext context) {
+//    return Container(
+//      color: this.bgColor,
+//      child: Column(
+//        children: [
+//          if(isMob && this.title != null)...{
+//            Container(
+//              width: double.infinity,
+//              height: 60,
+//              child: Row(
+//                children: [
+//                  IconButton(
+//                    icon: Icon(Icons.menu),
+//                    onPressed: (){
+//                      Navigator.of(context).pop(null);
+//                    },
+//                  ),
+//                  if(this.title != null)...{
+//                    const SizedBox(
+//                      width: 10,
+//                    ),
+//                    this.title!,
+//                  }
+//                ],
+//              ),
+//            )
+//          },
+//          Expanded(
+//            child: ListView(
+//              children: [
+//                for(var item in  this.items)...{
+//                  Container(
+//                    width: double.infinity,
+//                    height: 60,
+//                    child: MaterialButton(
+//                      onPressed: (){
+//                        if(item.content != null){
+//                          if(isMob){
+//                            Navigator.of(context).pop(null);
+//                          }
+//                          ScaffoldZ().chooseContentFromDrawer(context, content: item.content!);
+//                        }
+//                      },
+//                      child: Row(
+//                        children: [
+//                          if(item.icon != null)...{
+//                            Icon(item.icon),
+//                            const SizedBox(
+//                              width: 10,
+//                            )
+//                          },
+//                          Expanded(
+//                            child: item.title,
+//                          ),
+//                          if(item.sub_content != null)...{
+//                            Icon(Icons.keyboard_arrow_right)
+//                          }
+//                        ],
+//                      ),
+//                    ),
+//                  ),
+//                  if(item.sub_content != null)...{
+//                    for(var sub_item in item.sub_content!)...{
+//                      Container(
+//                        padding: const EdgeInsets.symmetric(horizontal: 10),
+//                        width: double.infinity,
+//                        height: 60,
+//                        child: MaterialButton(
+//                          onPressed: (){
+//                            if(sub_item.content != null){
+//                              if(isMob){
+//                                Navigator.of(context).pop(null);
+//                              }
+//                              ScaffoldZ().chooseContentFromDrawer(context, content: item.content!);
+//                            }
+//                          },
+//                          child: Row(
+//                            children: [
+//                              if(sub_item.icon != null)...{
+//                                Icon(sub_item.icon),
+//                                const SizedBox(
+//                                  width: 10,
+//                                )
+//                              },
+//                              Expanded(
+//                                child: sub_item.title,
+//                              )
+//                            ],
+//                          ),
+//                        ),
+//                      ),
+//                    }
+//                  }
+//                }
+//              ],
+//            ),
+//          )
+//        ],
+//      ),
+//    );
+//  }
+//}
